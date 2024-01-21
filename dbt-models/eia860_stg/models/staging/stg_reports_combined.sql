cbb20dc17fd725bb507a15ff76038eb06dc58eef
Chinelo Agbim <cnagbim@gmail.com>
2024-01-21 15:20:38 -0700

    added generic tests to primary id
    unit add to id and generic testing added

--- dbt-models/eia860_stg/models/staging/stg_reports_combined.sql
+++ dbt-models/eia860_stg/models/staging/stg_reports_combined.sql
@@ -1,21 +1,21 @@
 With y2023 as (
-    select *, Concat(plant_id, '-', report_date) as report_plant_id 
+    select *, Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id 
     from {{source('raw','reports_2023')}}
 ),
 y2022 as(
-    select * , Concat(plant_id, '-', report_date) as report_plant_id
+    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
     from {{source('raw','reports_2022')}}
 ),
 y2021 as (
-    select * , Concat(plant_id, '-', report_date) as report_plant_id 
+    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id 
     from {{source('raw','reports_2021')}}
 ),
 y2020 as (
-    select * , Concat(plant_id, '-', report_date) as report_plant_id
+    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
     from {{source('raw','reports_2020')}}
 ),
 y2019 as (
-    select * , Concat(plant_id, '-', report_date) as report_plant_id
+    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
     from {{source('raw','reports_2019')}}
 )
 
