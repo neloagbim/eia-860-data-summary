ed2050b0c7ce2c329c19d2eaa818fbfd37992960
Chinelo Agbim <cnagbim@gmail.com>
2024-01-21 15:04:05 -0700

    changed file path and project name
    cannot have 2 targets in postgres dbt project. this project will focus on transforming raw and moving it to staging

--- dbt-models/eia860_stg/models/staging/stg_reports2.sql
+++ dbt-models/eia860_stg/models/staging/stg_reports2.sql
@@ -1,10 +1,10 @@
 With y2023 as (
     select *, Concat(plant_id, '-', report_date) as report_plant_id 
-    from {{source('eia860_stg','reports_2023')}}
+    from {{source('raw','reports_2023')}}
 ),
 y2022 as(
     select * , Concat(plant_id, '-', report_date) as report_plant_id
-    from {{source('eia860_stg','reports_2022')}}
+    from {{source('raw','reports_2022')}}
 )
 select * from y2023
 union all
--- dbt-models/eia860_stg/models/staging/stg_reports_combined.sql
+++ dbt-models/eia860_stg/models/staging/stg_reports_combined.sql
@@ -1,22 +1,22 @@
 With y2023 as (
     select *, Concat(plant_id, '-', report_date) as report_plant_id 
-    from raw.eia860.reports_2023
+    from {{source('raw','reports_2023')}}
 ),
 y2022 as(
     select * , Concat(plant_id, '-', report_date) as report_plant_id
-    from raw.eia860.reports_2022
+    from {{source('raw','reports_2022')}}
 ),
 y2021 as (
     select * , Concat(plant_id, '-', report_date) as report_plant_id 
-    from raw.eia860.reports_2021
+    from {{source('raw','reports_2021')}}
 ),
 y2020 as (
     select * , Concat(plant_id, '-', report_date) as report_plant_id
-    from raw.eia860.reports_2020
+    from {{source('raw','reports_2020')}}
 ),
 y2019 as (
     select * , Concat(plant_id, '-', report_date) as report_plant_id
-    from raw.eia860.reports_2019
+    from {{source('raw','reports_2019')}}
 )
 
 select * from y2023
