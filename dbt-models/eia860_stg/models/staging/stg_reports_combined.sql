97e51a528c2a68f36624d621f4637b5fe38d244a
Chinelo Agbim <cnagbim@gmail.com>
2024-01-26 14:32:44 -0700

    Update stg_reports_combined.sql
    created proper surrogate key

--- dbt-models/eia860_stg/models/staging/stg_reports_combined.sql
+++ dbt-models/eia860_stg/models/staging/stg_reports_combined.sql
@@ -1,24 +1,25 @@
 With y2023 as ( --create CTE for each report year
-    select *, Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id 
+    select *--, Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id 
     from {{source('raw','reports_2023')}} --reference source 2023 table in raw schema
 ),
 y2022 as(
-    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
+    select * --, Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
     from {{source('raw','reports_2022')}}
 ),
 y2021 as (
-    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id 
+    select * --, Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id 
     from {{source('raw','reports_2021')}}
 ),
 y2020 as (
-    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
+    select * --, Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
     from {{source('raw','reports_2020')}}
 ),
 y2019 as (
-    select * , Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
+    select * --, Concat(plant_id, '-', unit_id, '-',report_date) as report_plant_id
     from {{source('raw','reports_2019')}}
-)
+),
 -- union all CTEs making sure to keep all records
+y_all as (
 select * from y2023
 union all
 select * from y2022
@@ -27,4 +28,7 @@ select * from y2021
 union all
 select * from y2020
 union all
-select * from y2019
+select * from y2019)
+-- add surrogate key to combined table
+select *, {{dbt_utils.generate_surrogate_key(['plant_id','unit_id','report_date'])}} as report_plant_id
+from y_all
